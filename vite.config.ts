e1017c2d610ff6181bbf15a0ee31e3a51336d6c9
LeeJams <89899636+LeeJams@users.noreply.github.com>
2022-04-02 09:17:46 +0900

    배포 이슈 수정..

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,28 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  build: {
-    base: "/codestates-bithumb-frontend",
-    outDir: "docs",
-  },
-});
+export default ({ command, mode }: any) => {
+  if (command === "serve") {
+    return {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+    };
+  } else {
+    return {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+      base: "/codestates-bithumb-frontend/",
+      build: {
+        outDir: "docs",
+      },
+    };
+  }
+};
